82b20fd17e8082303bd767ca1e0ad082c288e854
Angelo Rodriguez <angelo.rodriguez@cancom.de>
2026-01-27 12:18:47 +0000

    chore: add devshell

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Schulprojekt";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    mdq = {
+      url = "git+https://git.hydrar.de/mdtools/mdq";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, mdq }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          pkgs.curl
+          pkgs.unzip
+          pkgs.typst
+          mdq.packages.${system}.default
+        ];
+      };
+    };
+}
